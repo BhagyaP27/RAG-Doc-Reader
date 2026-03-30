99bcf27bc4573256d30f1e4afbfb7744c2c9092b
Bhagya Patel <bhagyapatel2077@gmail.com>
2026-03-30 19:43:47 -0400

    feat(infra): a entry point, manually run this file to create a S3 bucket and DynamoDB table so future terraform init reads from them automatically

--- infra/main.tf
+++ infra/main.tf
@@ -1,1 +1,15 @@
-"provider + S3 state backend"
+terraform{
+    required_providers {
+        aws = {source = "hashicorp/aws", version = "~> 5.0"}
+    }
+
+    backend "s3" {
+        bucket = "rag-doc-reader-tf-state"
+        key = "prod/terraform.tfstate"
+        region = "us-east-1"
+        dynamodb_table = "rag-tf-lock" # prevents concurrent apply corruption
+        encrypt = true
+    }
+}
+
+provider "aws" { region = var.aws_region}
